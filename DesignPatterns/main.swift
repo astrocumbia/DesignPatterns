88b6e08d521d3d03831507767ae39c099344c36c
positr0nix <irvin.py@gmail.com>
2018-01-10 17:55:24 -0600

    Initial Commit

---
+++ DesignPatterns/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DesignPatterns
+//
+//  Created by Irvin  Castellanos on 1/10/18.
+//  Copyright © 2018 Innku. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
